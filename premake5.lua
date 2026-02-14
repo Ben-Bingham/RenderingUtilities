e37f0764defbd8d83adb74b9ac2cd9b714de5bfe
Ben-Bingham <ben@benbingham.ca>
2026-02-13 21:41:34 -0700

    Remove deprecated premake feature

--- premake5.lua
+++ premake5.lua
@@ -3,7 +3,7 @@ project "RenderingUtilities"
     language "C++"
     cppdialect "C++20"
 
-	flags "MultiProcessorCompile"
+    multiprocessorcompile "on"
 
     filter "configurations:Debug"
         symbols "on"
